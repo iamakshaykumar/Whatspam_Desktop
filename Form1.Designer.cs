d9bb39777cbd745fde967446e449d176f4ba774e
Francesco Barabino <francesco.barabino@tiscali.it>
2020-05-16 21:08:13 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Whatspam
+{
+    partial class Whatspam
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Whatspam));
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.messageInput = new System.Windows.Forms.TextBox();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numberInput = new System.Windows.Forms.NumericUpDown();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numberInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.nameLabel.Font = new System.Drawing.Font("HACKED", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.YellowGreen;
+            this.nameLabel.Location = new System.Drawing.Point(11, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(134, 35);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "obiettivo";
+            this.nameLabel.MouseEnter += new System.EventHandler(this.nameLabel_MouseEnter);
+            this.nameLabel.MouseLeave += new System.EventHandler(this.nameLabel_MouseLeave);
+            // 
+            // nameInput
+            // 
+            this.nameInput.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.nameInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameInput.Font = new System.Drawing.Font("DEC Terminal Modern", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.ForeColor = System.Drawing.Color.OliveDrab;
+            this.nameInput.Location = new System.Drawing.Point(17, 65);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(384, 42);
+            this.nameInput.TabIndex = 1;
+            this.nameInput.Text = "Scrivi il nome del contatto";
+            this.nameInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nameInput_KeyDown);
+            this.nameInput.Leave += new System.EventHandler(this.nameInput_Leave);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.messageLabel.Font = new System.Drawing.Font("HACKED", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.ForeColor = System.Drawing.Color.YellowGreen;
+            this.messageLabel.Location = new System.Drawing.Point(11, 150);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(158, 35);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "Messaggio";
+            this.messageLabel.MouseEnter += new System.EventHandler(this.messageLabel_MouseEnter);
+            this.messageLabel.MouseLeave += new System.EventHandler(this.messageLabel_MouseLeave);
+            // 
+            // messageInput
+            // 
+            this.messageInput.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.messageInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messageInput.Font = new System.Drawing.Font("DEC Terminal Modern", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageInput.ForeColor = System.Drawing.Color.OliveDrab;
+            this.messageInput.Location = new System.Drawing.Point(17, 202);
+            this.messageInput.Name = "messageInput";
+            this.messageInput.Size = new System.Drawing.Size(361, 42);
+            this.messageInput.TabIndex = 3;
+            this.messageInput.Text = "Scrivi il messaggio";
+            this.messageInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.messageInput_KeyDown);
+            this.messageInput.Leave += new System.EventHandler(this.messageInput_Leave);
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.numberLabel.Font = new System.Drawing.Font("HACKED", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.ForeColor = System.Drawing.Color.YellowGreen;
+            this.numberLabel.Location = new System.Drawing.Point(11, 292);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(247, 35);
+            this.numberLabel.TabIndex = 4;
+            this.numberLabel.Text = "numero messaggi";
+            this.numberLabel.MouseEnter += new System.EventHandler(this.numberLabel_MouseEnter);
+            this.numberLabel.MouseLeave += new System.EventHandler(this.numberLabel_MouseLeave);
+            // 
+            // numberInput
+            // 
+            this.numberInput.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.numberInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numberInput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.numberInput.Font = new System.Drawing.Font("Virtual DJ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberInput.ForeColor = System.Drawing.Color.YellowGreen;
+            this.numberInput.Location = new System.Drawing.Point(17, 345);
+            this.numberInput.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numberInput.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numberInput.Name = "numberInput";
+            this.numberInput.Size = new System.Drawing.Size(156, 36);
+            this.numberInput.TabIndex = 5;
+            this.numberInput.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(584, 46);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(347, 367);
+            this.confirmButton.TabIndex = 6;
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar.Location = new System.Drawing.Point(17, 422);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(359, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // Whatspam
+            // 
+            this.AcceptButton = this.confirmButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(985, 473);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.numberInput);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.messageInput);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.nameLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1003, 520);
+            this.MinimumSize = new System.Drawing.Size(1003, 520);
+            this.Name = "Whatspam";
+            this.Text = "Whatspam";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Whatspam_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.numberInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox messageInput;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.NumericUpDown numberInput;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Whatspam
+{
+    public partial class Whatspam : Form
+    {
+        //Monitoring variable
+        bool browserOpen = false;
+        //Declaring chromedriver setup variables
+        IWebDriver driver;
+        ChromeDriverService driverService;
+        //Default error message when targets aren't found
+        string NOT_FOUND = "Contatto non trovato";
+
+        public Whatspam()
+        {
+            InitializeComponent();
+        }
+        
+        //When the confirm button gets clicked
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            IWebElement contact;
+            bool breakOut = false;
+            //Saving the target name from user input
+            string targetName = nameInput.Text;
+            Stopwatch restTime;
+
+            if (!browserOpen)
+            {
+                //If the browser isn't already open, it setups the driver and opens Whatsapp Web page
+                driverService = ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
+                //Prevents the Dev Tools command prompt from being displayed
+                driverService.HideCommandPromptWindow = true;
+
+                driver = new ChromeDriver(driverService);
+                
+                driver.Url = "https://web.whatsapp.com/";
+
+                browserOpen = true;
+            }
+            //Resets progress bar value
+            progressBar.Value = 0;
+            //Setting up a timer to give the user a limited amount of time to log in whatsapp
+            restTime = new Stopwatch();
+            restTime.Start();
+            //Waits until the user is logged in, or the time finishes
+            while (true)
+            {
+                try
+                {
+                    driver.FindElement(By.XPath($"//span[@class='_1wjpf _3NFp9 _3FXB1']"));
+                    break;
+                }
+                catch (NoSuchElementException)
+                {
+                    if (restTime.Elapsed.Seconds > 60)
+                    {
+                        //Closes the browser
+                        driver.Quit();
+                        breakOut = true;
+                        restTime.Stop();
+                        break;
+                    }
+                }
+            }
+
+            restTime.Stop();
+            //If the user is logged in
+            if (!breakOut)
+            {
+                try
+                {
+                    //Retrieves the target from recent chats and starts the spamming process
+                    contact = driver.FindElement(By.XPath($"//span[@class='_1wjpf _3NFp9 _3FXB1' and @title='{targetName}']"));
+
+                    SpamTarget(contact);
+                }
+                catch (NoSuchElementException)
+                {
+                    //Searches for the target in the Whatsapp search bar and then starts the spamming process
+                    driver.FindElement(By.XPath("//div[@class='_2S1VP copyable-text selectable-text' and @data-tab='3']")).SendKeys(targetName);
+
+                    try
+                    {
+                        contact = driver.FindElement(By.XPath($"//span[@class='_1wjpf _3NFp9 _3FXB1' and @title='{targetName}']"));
+
+                        SpamTarget(contact);
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        //If the target couldn't be found it minimizes the browser window and shows an error message
+                        driver.Manage().Window.Minimize();
+
+                        nameInput.Text = NOT_FOUND;
+                        nameInput.ForeColor = Color.Red;
+                    }  
+                }
+            }           
+        }
+
+        //Spamming method
+        private void SpamTarget(IWebElement contact)
+        {
+            string message;
+            int spamNum;
+            int i;
+            //Opens the chat of the given target
+            contact.Click();
+            //Locates the message input space
+            IWebElement textField = driver.FindElement(By.XPath("//div[@class='_2S1VP copyable-text selectable-text' and @data-tab='1']"));
+            //Saves the message and the number of messages to be sent from user input
+            message = messageInput.Text;
+            spamNum = (int)numberInput.Value;
+            //Sets a maximum for the progress bar
+            progressBar.Maximum = spamNum;
+            //Sends the same message n times
+            for (i = 0; i < spamNum; i++, progressBar.Value++)
+            {
+                textField.SendKeys(message);
+
+                driver.FindElement(By.ClassName("_35EW6")).Click();
+            }           
+        }
+
+        //Closes the browser when the program is closed
+        private void Whatspam_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (browserOpen)
+            {
+                driver.Quit();
+            }          
+        }
+        
+
+        #region DEFAULT TEXT
+        private void nameInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (nameInput.Text == "Scrivi il nome del contatto" || nameInput.Text == NOT_FOUND)
+            {
+                nameInput.Text = "";
+                nameInput.ForeColor = Color.YellowGreen;
+            }
+        }
+
+        private void messageInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (messageInput.Text == "Scrivi il messaggio")
+            {
+                messageInput.Text = "";
+                messageInput.ForeColor = Color.YellowGreen;
+            }          
+        }
+
+        private void nameInput_Leave(object sender, EventArgs e)
+        {
+            if (nameInput.Text == "")
+            {
+                nameInput.Text = "Scrivi il nome del contatto";
+                nameInput.ForeColor = Color.OliveDrab;
+            }
+        }
+
+        private void messageInput_Leave(object sender, EventArgs e)
+        {
+            if (messageInput.Text == "")
+            {
+                messageInput.Text = "Scrivi il messaggio";
+                messageInput.ForeColor = Color.OliveDrab;
+            }
+        }
+        #endregion
+
+        #region HIGHLIGHT
+
+        private void nameLabel_MouseEnter(object sender, EventArgs e)
+        {
+            nameLabel.ForeColor = System.Drawing.Color.GreenYellow;
+        }
+
+        private void nameLabel_MouseLeave(object sender, EventArgs e)
+        {
+            nameLabel.ForeColor = System.Drawing.Color.YellowGreen;
+        }
+
+        private void messageLabel_MouseEnter(object sender, EventArgs e)
+        {
+            messageLabel.ForeColor = Color.GreenYellow;
+        }
+
+        private void messageLabel_MouseLeave(object sender, EventArgs e)
+        {
+            messageLabel.ForeColor = Color.YellowGreen;
+        }
+
+        private void numberLabel_MouseEnter(object sender, EventArgs e)
+        {
+            numberLabel.ForeColor = Color.GreenYellow;
+        }
+
+        private void numberLabel_MouseLeave(object sender, EventArgs e)
+        {
+            numberLabel.ForeColor = Color.YellowGreen;
+        }
+
+        #endregion
+
+        #region UTILITIES
+        private bool isIn(string[] arr, string str)
+        {
+            foreach (string s in arr)
+            {
+                if (s == str)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+    }
+}
